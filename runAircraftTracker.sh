7da9662c510100702a42b64a34384cc6bd96cb1e
biohead <sfalkiner@gmail.com>
2021-06-17 18:47:19 +0100

    initial dev branch

--- runAircraftTracker.sh
+++ runAircraftTracker.sh
@@ -3,18 +3,18 @@
 # This is just a simple script that loads the correct python enviroment
 # and then loops forever restarting the tracker program if it ever exits.
 
-cd /home/pi/AboveMonroe
+cd /home/pi/AboveBracknell
 
 while :
 do
 	echo
-	echo "*** Starting [AboveMonroe] $(date) ***"
+	echo "*** Starting [AboveBracknell] $(date) ***"
 	echo
 
 	python3 AircraftTracker.py
 
 	echo
-	echo "*** Exiting  [AboveMonroe] $(date) ***"
+	echo "*** Exiting  [AboveBracknell] $(date) ***"
 	echo
 
 	sleep 5
